406a420b4e22b3cbf1a4bcd66309f69e105602a7
Vikt0r.Nekrutenko <vikt0r.nekrut@outlook.com>
2022-09-13 14:52:58 +0300

    015

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
+INC=-I../stf -I../stf/sdb -I../stf/smv
+
 all:
-	#clang++ -fpic -shared stf/*.cpp -o ~/libstf.so
-	clang++ ~/libstf.so tik_tac_toe/*.cpp main.cpp -o ~/main
-	~/main
+	clang++ ~/libstf.so $(INC) *.cpp -o ~/tictactoe
+	~/tictactoe
--- main.cpp
+++ main.cpp
@@ -1,76 +1,24 @@
-#include <ctime>
-#include <unistd.h>
+#include "window.hpp"
 
-#include "stf/utils.hpp"
-//#include "stf/renderer.hpp"
-
-#include "tik_tac_toe/view.hpp"
-#include "tik_tac_toe/model.hpp"
-
-class Game
+class Game : public stf::Window
 {
-    stf::Renderer rend;
-    View view;
-    public:
+  public:
+  
+  bool onUpdate(const float dt)
+	{
+	  return true; 
+	}
+	void keyEvents(const int key)
+	{ 
     
-    Game(const uint8_t w, const uint8_t h) 
-        : rend(w, h),
-        view(new Model)
-    {
-        
-    }
-    
-    void update(const float dt)
-    {
-      view.show(rend);
-    }
-    
-    void keyEvents(const int key)
-    {
-      view.keyEvents(key);
-    }
-    
-    int run()
-    {
-        float dt = 0.f, elpsdTime = 0.f, fps = 0.f;
-        uint32_t cntdFrms = 0u;
-        
-        fputs("\e[?25l", stdout);
-        while(true)
-        {
-            if (elpsdTime > 1.f)
-            {
-                fps = cntdFrms / elpsdTime;
-                elpsdTime = cntdFrms = 0;
-            }
-            int sym = 0;
-            time_t begin = clock();
-            stf::set_mode(1);
-            if((sym = stf::get_key()))
-            {
-                if(sym == 'q') 
-                {
-                    break;
-                }
-                keyEvents(sym);
-            }
-            usleep(10000);
-            rend.clear();
-            rend.drawNumber(1, 1, fps, stf::Color::lgrey);
-            update(dt);
-            rend.display();
-            
-            dt = (clock() - begin) / 1000000.f;
-            elpsdTime += dt;
-            cntdFrms++;
-        }
-        return 0;
-    }
+	}
+	void mouseEvents(const stf::MouseRecord &mr)
+	{
+	  
+	}	
 };
 
 int main()
 {
-  winsize ws = stf::getws();
-	Game game(ws.ws_col, ws.ws_row);
-	return game.run();
+  return Game().run();
 }
