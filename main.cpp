26c3bb8abefaaf79875a634f18cbb504d75d4194
Vikt0r.Nekrutenko <vikt0r.nekrut@outlook.com>
2022-09-13 16:59:30 +0300

    017

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ using namespace stf::smv;
 class GameModel : public BaseModel
 {
   public:
-  
+    const uint8_t* board() const { return m_board; }
   private:
-    uint8_t m_board[9];
+    uint8_t m_board[9] { 'u','u','u','u','u','u','u','u','u' };
     uint8_t m_player = 'x';
     uint8_t m_curCell = 0;
 };
@@ -23,7 +23,15 @@ class GameView : public IView
   
   void show(Renderer& renderer) final
   {
-    m_board.show(renderer, renderer.Size / 2 - m_board.Size() / 2);
+    Vec2d pzero = renderer.Size / 2 - m_board.Size() / 2;
+    m_board.show(renderer, pzero);
+    
+    for(int y = 0; y < 3; ++y) {
+      for(int x = 0; x < 3; ++x) {
+        uint8_t sym = m_gameModel->board()[3 * y + x];
+        renderer.drawPixel(Vec2d(x,y) * Vec2d(4, 2) + pzero +Vec2d(1, 0), sym);
+      }
+    }
   }
     
   private:
