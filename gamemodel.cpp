4e7d60de957a4ce736e9a39acaa2a3b21d144365
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-10-19 18:16:43 +0300

    134

--- gamemodel.cpp
+++ gamemodel.cpp
@@ -3,28 +3,6 @@
 #include "pausemenuview.hpp"
 #include <fstream>
 
-void GameSaveModel::save()
-{
-  for(auto i = 0; i < 9; ++i)
-    board[i] = m_model->m_board[i];
-  xcursor = m_model->m_cursor.pos.x;
-  ycursor = m_model->m_cursor.pos.y;
-  player  = m_model->m_cursor.sym;
-  
-  push<GameSaveModel>();
-}
-
-void GameSaveModel::load()
-{
-  pop<GameSaveModel>();
-  
-  for(auto i = 0; i < 9; ++i)
-    m_model->m_board[i] = board[i];
-  m_model->m_cursor.pos.x = xcursor();
-  m_model->m_cursor.pos.y = ycursor();
-  m_model->m_cursor.sym = player();
-}
-
 GameModel::GameModel(GameResultInfoModel* model)
   : m_story{ model }
 { 
